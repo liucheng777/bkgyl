78955cd4c70b70839ec5272f6ed9c686e2dbe61a
刘施施 <liushishi577@163.com>
2016-10-08 17:15:45 +0800

    10.8.4

---
+++ bkgyl/首页/View/SpaceTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SpaceTableViewCell.h
+//  bkgyl
+//
+//  Created by 黄诸龙 on 16/10/8.
+//  Copyright © 2016年 刘施施. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpaceTableViewCell : UITableViewCell
+
+@end
---
+++ bkgyl/首页/View/SpaceTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SpaceTableViewCell.m
+//  bkgyl
+//
+//  Created by 黄诸龙 on 16/10/8.
+//  Copyright © 2016年 刘施施. All rights reserved.
+//
+
+#import "SpaceTableViewCell.h"
+
+@implementation SpaceTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
