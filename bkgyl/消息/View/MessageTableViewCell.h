80b23c22bc4b996627ba318176a701c3d8e91c00
刘施施 <liushishi577@163.com>
2016-10-08 15:57:12 +0800

    10.8.3

---
+++ bkgyl/消息/View/MessageTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MessageTableViewCell.h
+//  bkgyl
+//
+//  Created by 黄诸龙 on 16/10/8.
+//  Copyright © 2016年 刘施施. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageTableViewCell : UITableViewCell
+
+@end
