f6577e1e908af807cc097a678a81f4ab547f8fbc
刘施施 <liushishi577@163.com>
2016-10-10 09:02:41 +0800

    10.10.1

---
+++ bkgyl/首页/View/GoodsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  GoodsTableViewCell.h
+//  bkgyl
+//
+//  Created by 黄诸龙 on 16/10/10.
+//  Copyright © 2016年 刘施施. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoodsTableViewCell : UITableViewCell
+
+@end
---
+++ bkgyl/首页/View/GoodsTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  GoodsTableViewCell.m
+//  bkgyl
+//
+//  Created by 黄诸龙 on 16/10/10.
+//  Copyright © 2016年 刘施施. All rights reserved.
+//
+
+#import "GoodsTableViewCell.h"
+
+@implementation GoodsTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
